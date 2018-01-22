b4a42c11adef0544442e535d68b7b1826c7a9643
Pradeep Yarlagadda <sreeramsaipradeep@gmail.com>
2018-01-23 00:08:00 +0530

    Updated server.c

--- server.c
+++ server.c
@@ -1,29 +1,31 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <netinet/in.h>
 #define PORT 8080
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int server_fd, new_socket, valread;
-    struct sockaddr_in address;  // sockaddr_in - references elements of the socket address. "in" for internet
+    // sockaddr_in - references elements of the socket address. "in" for internet
+    struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
 
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  // creates socket, SOCK_STREAM is for TCP. SOCK_DGRAM for UDP
-    {
+    // creates socket, SOCK_STREAM is for TCP. SOCK_DGRAM for UDP
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
     // This is to lose the pesky "Address already in use" error message
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt))) // SOL_SOCKET is the socket layer itself
-    {
+    // SOL_SOCKET is the socket layer itself
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -32,36 +34,75 @@ int main(int argc, char const *argv[])
     address.sin_port = htons( PORT );    // Server port to open. Htons converts to Big Endian - Left to Right. RTL is Little Endian
 
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
-    {
+    if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
     // Port bind is done. You want to wait for incoming connections and handle them in some way.
     // The process is two step: first you listen(), then you accept()
-    if (listen(server_fd, 1) < 0) // 3 is the maximum size of queue - connections you haven't accepted
-    {
+    // 3 is the maximum size of queue - connections you haven't accepted
+    if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
     while(1) {
         // returns a brand new socket file descriptor to use for this single accepted connection. Once done, use send and recv
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                           (socklen_t*)&addrlen))<0)
-        {
-            perror("accept");
-            exit(EXIT_FAILURE);
+        struct sockaddr_in cliaddr;
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&cliaddr,(socklen_t*)&addrlen))<0) {
+            perror("Error in Accepting New Connection");
+            close(new_socket);
+            continue;
         }
-        // printf("Connection Established from %s",inet_ntoa(address->sin_addr));
-        valread = read( new_socket , buffer, 1024);  // read infromation received into the buffer
-        printf("%s\n",buffer );
-        send(new_socket , hello , strlen(hello) , 0 );  // use sendto() and recvfrom() for DGRAM
-        printf("Hello message sent\n");
-        while(1);
-        close(new_socket);
+
+        // Reading file name requested by client.
+        // read infromation received into the buffer
+        valread = read(new_socket , buffer, 1024);
+        if(valread == -1) {
+            perror("failed to read filename from client");
+            close(new_socket);
+            continue;
+        }
+        buffer[valread] = '\0';
+
+        // Opening the file requested by the client.
+        int fd;
+        if ( (fd= open(buffer,O_RDONLY)) < 0) {
+            perror("failed to open the requested file");
+            close(fd);
+            close(new_socket);
+            continue;
+        }
+
+        int read_status = 1;
+
+        valread = read(fd,buffer,1024);
+        if (valread == -1) {
+            perror("Failed to read the requested file");
+            close(fd);
+            close(new_socket);
+            continue;
+        }
+
+        // Read 1024 bytes at once and sending to the client.
+        while(valread) {
+            send(new_socket,buffer,valread,0);
+            valread = read(fd,buffer,1024);
+            if (valread == -1) {
+                perror("Failed to read the requested file");
+                close(fd);
+                close(new_socket);
+                read_status = -1;
+                break;
+            }            
+        }
+
+        if(read_status != -1) {
+            printf("File successfully sent to the client\n");
+            close(new_socket);
+        }
     }
+
     return 0;
 }
