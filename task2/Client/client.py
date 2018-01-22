dd6e3fadd257e5e8935e9860f56a70cfd75279f0
Subramanyam <subramanyam.kmk03@gmail.com>
2018-01-23 02:26:27 +0530

    modified client.py for task2

--- task2/Client/client.py
+++ task2/Client/client.py
@@ -4,21 +4,27 @@ s = socket.socket()
 host = ""
 port = 60001                
 
-s.connect((host, port))
-s.send("Hello server!")
+s.connect(('10.42.0.106', port))
+# s.send("Hello server!")
+while True:
+	filename = raw_input('Enter a filename ')
+	s.send(filename)
+	if filename == 'end':
+		break
+	with open(filename, 'wb') as f:
+	    print 'file opened'
+	    while True:
+	        print('receiving data...')
+	        data = s.recv(1024)
+	        code = data[0:3]
+	        if code == '500':
+	        	break
+	        data = data[3:]
+	        print('data=%s', (data))
+	        # write data to a file
+	        f.write(data)
 
-with open('received_file', 'wb') as f:
-    print 'file opened'
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
-
-f.close()
-print('Successfully get the file')
+	f.close()
+	print('Successfully get the file')
 s.close()
 print('connection closed')
