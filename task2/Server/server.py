bba6994ec275e905082963ffe1e0948ea2f341e2
Pradeep Yarlagadda <sreeramsaipradeep@gmail.com>
2018-01-23 02:26:33 +0530

    Modified server.py with persistent connection

--- task2/Server/server.py
+++ task2/Server/server.py
@@ -7,24 +7,29 @@ host = ""
 s.bind((host, port))
 s.listen(5)
 
-filename = raw_input("Enter file to share:")
+read_len = 1024 - 5
+
+# filename = raw_input("Enter file to share:")
 print 'Server listening....'
 
 while True:
     conn, addr = s.accept()
     print 'Got connection from', addr
-    data = conn.recv(1024)
-    print data
-    print('Server received', repr(data))
 
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
+    while True:
+        filename = conn.recv(1024)
 
-    print('Done sending')
-    conn.send('Thank you for connecting')
+        # Client closed the connection. handel code
+        if filename == "end":
+            break
+
+        f = open(filename,'rb')
+        l = f.read(read_len)
+        while (l):
+           conn.send("300"+l)
+           print('Sent ',repr(l))
+           l = f.read(read_len)
+        conn.send("500")
+        f.close()
     conn.close()
+    print 'Connection closed'
